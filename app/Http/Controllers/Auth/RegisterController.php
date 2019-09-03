87c8d5e4f7faa1b91869e9f6623a386e2f2a3198
facundo martin <facundo.martin.9@gmail.com>
2019-09-03 19:04:11 -0300

    Auto stash before merge of "master" and "origin/master"

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,6 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
       return Validator::make($data,[
+
         'name'=>['required','string','max:255'],
         'apellido'=>['required','string','max:255'],
         'domicilio'=>['required','string','max:255'],
