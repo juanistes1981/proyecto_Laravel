8f2844eff7b0dcf34a8339f797ced54139080fbb
Irina Soria <damarisirinasoria@gmail.com>
2019-09-11 17:18:24 -0300

    faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -15,8 +15,8 @@
     height: 17em;
     margin-top: 3em;">
     </div>
-          <div class="container-faq">
-              <h3>¿Cuáles son los medios de pago aceptados?</h3>
+          <div class="container-faq" >
+              <h3 style="height:25%;">¿Cuáles son los medios de pago aceptados?</h3>
                     <p>Los medios de pago aceptados son:</p>
 
                     <p>-Tarjetas de Débito y Credito Visa o Mastercard</p>
@@ -25,25 +25,25 @@
             </div>
 
           <div class="container-faq">
-                <h3>¿Cuánto cuesta el envío?</h3>
+                <h3 style="height:20%;">¿Cuánto cuesta el envío?</h3>
 
                   <p>El envio se calcula segun la cantidad de kilometros. En el momento que pidas el producto se te mostrara un estimado del costo.</p>
         </div>
 
         <div class="container-faq">
-              <h3>¿En cuánto tiempo recibo mi pedido?</h3>
+              <h3 style="height:20%;">¿En cuánto tiempo recibo mi pedido?</h3>
 
                 <p>El tiempo maximo estimado es de 10 días dependiendo de la localidad.  En caso que desee recir el pedido en una menor cantidad de tiempo debera abonar una tarifa diferente </p>
         </div>
 
         <div class="container-faq">
-                <h3>Me gustaría enviar un regalo, ¿es posible?</h3>
+                <h3 style="height:22%;">Me gustaría enviar un regalo, ¿es posible?</h3>
 
                 <p>Por supuesto! no pierdas la oportuniad de hacer el mejor regalo. Indicala dirección y recibiras la factura en tu email.</p>
         </div>
 
         <div class="container-faq">
-              <h3> No encuentro la respuesta a mi pregunta, ¿qué hago?</h3>
+              <h3 style="height:26%;"> No encuentro la respuesta a mi pregunta, ¿qué hago?</h3>
 
               <p>Haga clic en la página de "Contactos" y envíenos su pregunta, le responderemos tan pronto como sea posible.</p>
         </div>
