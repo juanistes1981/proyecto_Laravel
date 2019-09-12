c3e6516820100f81abcbd82ab1f7b0a1b1664d7b
facundo martin <facundo.martin.9@gmail.com>
2019-09-11 23:56:49 -0300

    buscar funcionando (faltaría el estilo)

--- routes/web.php
+++ routes/web.php
@@ -28,15 +28,17 @@ Route::get('/home', 'HomeController@home');
 Route::get('/music', 'musicController@all');
 Route::get('/book', 'booksController@all');
 Route::get('/categories', 'CategoriesController@all');
+
 Route::get('/pedidos', 'MusicController@detalle');
 Route::get('/pedidos', 'BooksController@detalle');
 Route::get('/pedidos', 'categoriesController@detalle');
 Route::get('/cart', 'cartsController@cart');
 Route::get("/addtocart/{id}", "cartsController@addToCart");
 
+Route::get('/category/{id}', 'CategoriesController@detail');
+
 Route::get('/faq','FaqController@faq');
 Route::get('/contacto','ContactoController@contacto');
-Route::get('/search', 'musicController@searchmusic');
 Route::get('/search', 'booksController@search');
 Route::get('/music/add', 'musicController@add')->middleware("auth");
 Route::get('/book/add', 'booksController@add')->middleware("auth");
